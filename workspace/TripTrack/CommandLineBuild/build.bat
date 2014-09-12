b6d46a74150c1ea565c5b3bfe8c91689b8928417
jprazak <jprazak@492e496e-58ca-f445-8ef8-0a43168f6385>
2014-09-12 02:02:41 +0000

    configured build.bat file for server
    git-svn-id: https://blocshop.cz:8040/svn/triptracktizen/trunk@61 492e496e-58ca-f445-8ef8-0a43168f6385

--- workspace/TripTrack/CommandLineBuild/build.bat
+++ workspace/TripTrack/CommandLineBuild/build.bat
@@ -1,2 +1,4 @@
 native-make -a i386 -t LLVM-3.1 -n "Tizen Emulator 2.2" -v "Tizen 2.2"
-native-packaging -ak D:\Developer\keyz\tizen\prazak-blocshop.p12 -ap Letmein123
+native-packaging -ak C:\tizen\tizen-sdk-data\keystore\author\prazak-blocshop.p12 -ap Letmein123
+'sdb connect 192.168.1.130
+sdb install i4CPJJOods-1.0.0-i386.tpk
