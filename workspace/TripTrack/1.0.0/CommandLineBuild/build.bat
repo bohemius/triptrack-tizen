cc8655609340abb75bc666a7a41944abc9ad0dfd
Jirka Prazak <bohemius@gmail.com>
2014-10-30 16:05:30 +0100

    Cleaning up subversion tag dirs and import duplicates

--- workspace/TripTrack/1.0.0/CommandLineBuild/build.bat
+++
@@ -1,4 +0,0 @@
-native-make -a i386 -t LLVM-3.1 -n "Tizen Emulator 2.2" -v "Tizen 2.2"
-native-packaging -ak C:\tizen\tizen-sdk-data\keystore\author\prazak-blocshop.p12 -ap Letmein123
-'sdb connect 192.168.1.130
-sdb install i4CPJJOods-1.0.0-i386.tpk
